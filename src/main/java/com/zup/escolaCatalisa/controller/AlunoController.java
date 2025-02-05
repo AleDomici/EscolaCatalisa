7a62386d4cc65c26c1ed6a81b1a1e51b45fbdeeb
[AleDomici] <alessandra.domiciano@zup.com.br>
2025-02-05 17:11:54 -0300

    feat(controller): adiciona endpoint POST para criar novos alunos e organiza mapeamento de rotas
    Adicionado o endpoint POST no AlunoController para permitir o cadastro de novos alunos.
    Além disso, foi incluída a anotação @RequestMapping para organizar o mapeamento de rotas no controlador.

--- src/main/java/com/zup/escolaCatalisa/controller/AlunoController.java
+++ src/main/java/com/zup/escolaCatalisa/controller/AlunoController.java
@@ -3,20 +3,29 @@ package com.zup.escolaCatalisa.controller;
 import com.zup.escolaCatalisa.model.Aluno;
 import com.zup.escolaCatalisa.repository.AlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/alunos")
 public class AlunoController {
 
     @Autowired
     private AlunoRepository alunoRepository;
 
-    @GetMapping("/alunos")
-    public List<Aluno> listarAluno() {
+    @GetMapping
+    public List<Aluno> listarAlunos() {
         return alunoRepository.findAll();
     }
 
+    // Endpoint POST para criar um novo aluno
+    @PostMapping
+    public ResponseEntity<String> criarAluno(@RequestBody Aluno aluno) {
+        alunoRepository.save(aluno);
+        return new ResponseEntity<>("Aluno cadastrado com sucesso", HttpStatus.CREATED);
+    }
 }
